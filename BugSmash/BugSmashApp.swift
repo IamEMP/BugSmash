de41c2c0ee946a05d39fac383ed4a8cff53c009a
Ethan Phillips <ethansemail@yahoo.com>
2023-05-08 21:40:08 -0500

    Initial Commit

---
+++ BugSmash/BugSmashApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugSmashApp.swift
+//  BugSmash
+//
+//  Created by Ethan Phillips on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BugSmashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
